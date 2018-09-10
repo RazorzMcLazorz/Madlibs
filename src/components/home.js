b5692e2147227469832c1954dc2cab9a5c1899db
NathanielYoung <young.nathaniel95@gmail.com>
2018-09-10 17:57:18 -0600

    make it better

--- src/components/home.js
+++ src/components/home.js
@@ -6,13 +6,17 @@ import Card from './card';
 class Home extends Component {
   render() {
     return (
-      <div className='home'>
+      <div className="home">
         { Header() }
         <Card />
-        
       </div>
     );
   }
 }
+
+// things we need to fix
+
+// content labels
+// generate btn space/card-height
 
 export default Home;
